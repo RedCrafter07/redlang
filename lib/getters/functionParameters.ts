b3545dda5bde4c4dbedee2426920ad455d40eb2f
RedCrafter07 <github@redcrafter07.de>
2022-01-27 21:43:19 +0100

    Param parser syntax (custom)

--- lib/getters/functionParameters.ts
+++ lib/getters/functionParameters.ts
@@ -3,7 +3,7 @@ function getFunctionParameters(funcLine: string, funcName: string): any {
 	if (!funcLine.includes(funcName)) return params;
 
 	let paramString = funcLine.slice(funcName.length).split(';')[1].trim();
-	params = paramString.split(',').map(param => param.trim());
+	params = paramString.split(',,').map(param => param.trim());
 
 	return params;
 }
