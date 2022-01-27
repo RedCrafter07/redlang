2ea3688b7961974bcd7cb49fa9b11c842280f76f
RedCrafter07 <github@redcrafter07.de>
2022-01-27 19:32:19 +0100

    No line splitting

--- lib/checkers/variables.ts
+++ lib/checkers/variables.ts
@@ -4,21 +4,22 @@ import getVariableFromString from '../getters/variableFromString';
 import variableToOutput from '../outputs/variableToOutput';
 import arrayToString from '../utils/arrayToString';
 
-function checkForVariables(input: string) {
-	let lines = input.split('\n');
+function checkForVariables(lines: Array<string>) {
 	lines.forEach((line, index) => {
-		if (line.includes('§')) {
-			let { name, constant, value } = getVariableFromString(line);
+		if (!line.startsWith('#')) {
+			if (line.includes('§')) {
+				let { name, constant, value } = getVariableFromString(line);
 
-			let variableString = variableToOutput({ name, constant, value });
+				let variableString = variableToOutput({ name, constant, value });
 
-			lines[index] = variableString;
+				lines[index] = variableString;
+			}
+
+			// if (variables.includes(variable)) {
+			// 	throw new Error('Variable ' + variable + ' is defined multiple times!');
+			// }}
 		}
-
-		// if (variables.includes(variable)) {
-		// 	throw new Error('Variable ' + variable + ' is defined multiple times!');
-		// }
 	});
 
-	return arrayToString(lines, true);
+	return lines;
 }
