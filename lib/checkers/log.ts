d819d98454fde84203fed14efb03476d1c812fc4
RedCrafter07 <github@redcrafter07.de>
2022-01-27 19:32:54 +0100

    No line splitting

--- lib/checkers/log.ts
+++ lib/checkers/log.ts
@@ -1,22 +1,23 @@
 import arrayToString from '../utils/arrayToString';
 import getFunctionParameters from '../getters/functionParameters';
 
-function checkForLog(input: string): string {
-	let lines = input.split('\n');
+function checkForLog(lines: Array<string>) {
 	lines.forEach((line, index) => {
-		if (line.includes('out')) {
-			let arr = getFunctionParameters(line, 'out');
-			if (arr.length > 1) {
-				throw new Error('out function can only have one parameter');
-			} else if (arr.length == 1) {
-				lines[index] = `console.log(${arr[0]});`;
-			} else {
-				lines[index] = `console.log();`;
+		if (!line.startsWith('#')) {
+			if (line.startsWith('out')) {
+				let arr = getFunctionParameters(line, 'out');
+				if (arr.length > 1) {
+					throw new Error('out function can only have one parameter');
+				} else if (arr.length == 1) {
+					lines[index] = `console.log(${arr[0]});`;
+				} else {
+					lines[index] = `console.log();`;
+				}
 			}
 		}
 	});
 
-	return arrayToString(lines, true);
+	return lines;
 }
 
 export default checkForLog;
--- lib/checkers/statements.ts
+++ lib/checkers/statements.ts
@@ -1,34 +1,35 @@
 import arrayToString from '../utils/arrayToString';
 
-export default (input: string) => {
-	let lines = input.split('\n'); // split the code into lines
+export default (lines: Array<string>) => {
 	let nextLineIsStatement = false;
 	lines.forEach((line, index) => {
-		//checking if line includes an statement
-		if (nextLineIsStatement == true) {
-			if (line.startsWith('$')) {
-				lines[index] = '}';
-				nextLineIsStatement = false;
+		if (!line.startsWith('#')) {
+			//checking if line includes an statement
+			if (nextLineIsStatement == true) {
+				if (line.startsWith('$')) {
+					lines[index] = '}';
+					nextLineIsStatement = false;
+				}
 			}
-		}
-		if (line.startsWith('?')) {
-			//checking for if statement type (normal, else if, else)
-			if (line.startsWith('? ')) {
-				let statement = line.slice('? '.length).split(':')[0];
-				lines[index] = `if (${statement}) {`;
-				nextLineIsStatement = true;
-			} else if (line.startsWith('?x')) {
-				lines[index] = 'else {';
-				nextLineIsStatement = true;
-			} else if (line.startsWith('?& ')) {
-				let statement = line.slice('?& '.length).split(':')[0];
-				lines[index] = `else if (${statement}) {`;
-				nextLineIsStatement = true;
-			} else {
-				throw new Error('Error while compiling statement: No valid statement provided!\n' + line);
+			if (line.startsWith('?')) {
+				//checking for if statement type (normal, else if, else)
+				if (line.startsWith('? ')) {
+					let statement = line.slice('? '.length).split(':')[0];
+					lines[index] = `if (${statement}) {`;
+					nextLineIsStatement = true;
+				} else if (line.startsWith('?x')) {
+					lines[index] = 'else {';
+					nextLineIsStatement = true;
+				} else if (line.startsWith('?& ')) {
+					let statement = line.slice('?& '.length).split(':')[0];
+					lines[index] = `else if (${statement}) {`;
+					nextLineIsStatement = true;
+				} else {
+					throw new Error('Error while compiling statement: No valid statement provided!\n' + line);
+				}
 			}
 		}
 	});
 
-	return arrayToString(lines, true);
+	return lines;
 };
