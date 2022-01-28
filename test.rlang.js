ccacb55bcd0adeab7a3194c910458914213b1784
RedCrafter07 <github@redcrafter07.de>
2022-01-28 11:32:40 +0100

    compiled to js

---
+++ test.rlang.js
@@ -0,0 +1,16 @@
+const text = 'Woah, a test which will fail!';
+console.log(text);
+if (text == 'Woah, a test!') {
+console.log('Oyes!');
+}
+else if (text == 'Woah, a test which will fail!') {
+console.log('Are you sure about that?');
+}
+else {
+console.log('OnO');
+}
+console.log('');
+console.log('Look, I can count to ten!');
+for (let num = 0; num <= 10; num++) {
+console.log(num);
+}
